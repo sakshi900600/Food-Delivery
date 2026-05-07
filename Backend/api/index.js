b99e1a669f0b2529f7360e60c27daaafca16482e
Sakshi <139939188+sakshi900600@users.noreply.github.com>
2026-05-07 13:09:19 +0530

    fixed the file path.

--- Backend/api/index.js
+++ Backend/api/index.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import { connectDB } from '../config/db.js'
-import foodRouter from '../routes/FoodRoute.js'
+import foodRouter from '../routes/foodRoute.js'
 import userRouter from '../routes/userRoute.js'
 import cartRouter from '../routes/cartRoute.js'
 import orderRoute from '../routes/orderRoute.js'
